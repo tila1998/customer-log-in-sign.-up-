0731ee5cd181483fbc683f35445439022aef3dae
tila1998 <92199660+tila1998@users.noreply.github.com>
2024-05-28 15:35:10 +0530

    Create user.js

---
+++ user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  }
+});
+
+// Hash the password before saving
+UserSchema.pre('save', async function (next) {
+  if (this.isModified('password') || this.isNew) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
